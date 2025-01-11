84b693b7562c58d6366c41e7e50db492a28c1d6a
sodaytech <sodaytech@gmail.com>
2025-01-11 23:46:15 +0500

    ii

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
-export default async function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default async function RootLayout({}) {
   return (
     <div className='flex flex-col min-h-screen'>
       {/* <Header /> */}
